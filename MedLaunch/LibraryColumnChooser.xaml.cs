33a11f14f5ed3a92ef935e7be3084523fef5af99
ASNiVOR <coding@asnitech.co.uk>
2017-02-13 17:44:40 +0000

    Started work on games library column visibility configuration

---
+++ MedLaunch/LibraryColumnChooser.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MahApps.Metro.SimpleChildWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MedLaunch.Classes;
+using MedLaunch.Models;
+using MahApps.Metro.Controls.Dialogs;
+using MedLaunch.Extensions;
+
+namespace MedLaunch
+{
+    /// <summary>
+    /// Interaction logic for LibraryColumnChooser.xaml
+    /// </summary>
+    public partial class LibraryColumnChooser : ChildWindow
+    {
+        public LibraryColumnChooser()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseSec_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
