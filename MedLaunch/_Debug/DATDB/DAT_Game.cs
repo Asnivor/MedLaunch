3840265c578b8f61f9c840eff07b6242bd91ab65
ASNiVOR <coding@asnitech.co.uk>
2017-06-27 20:50:26 +0100

    Some initial DAT DB work

---
+++ MedLaunch/_Debug/DATDB/DAT_Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedLaunch._Debug.DATDB
+{
+    public class DAT_Game
+    {
+        public int gid { get; set; }
+        public string gameName { get; set; }
+        public int pid { get; set; }
+        public string year { get; set; }
+        public string publisher { get; set; }
+        public int gdbid { get; set; }
+
+
+        /// <summary>
+        /// return list of all top level games
+        /// </summary>
+        /// <returns></returns>
+        public static List<DAT_Game> GetGames()
+        {
+            using (var context = new AsniDATAdminDbContext())
+            {
+                var cData = (from g in context.DAT_Game
+                             select g);
+                return cData.ToList();
+            }
+        }
+    }
+}
---
+++ MedLaunch/_Debug/DATDB/DAT_Rom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedLaunch._Debug.DATDB
+{
+    public class DAT_Rom
+    {
+        public int rid { get; set; }
+        public string romName { get; set; }
+        public string country { get; set; }
+        public string language { get; set; }
+        public string developmentStatus { get; set; }
+        public string otherFlags { get; set; }
+        public string cloneOf { get; set; }
+        public string copyright { get; set; }
+        public string size { get; set; }
+        public string crc { get; set; }
+        public string md5 { get; set; }
+        public string sha1 { get; set; }
+        public string year { get; set; }
+        public string publisher { get; set; }
+        public int datProviderId { get; set; }
+
+    }
+}
