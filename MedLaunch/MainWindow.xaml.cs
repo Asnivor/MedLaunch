ec4a475882e578ce0f030f2adbe51efa8d4e477a
ASNiVOR <coding@asnitech.co.uk>
2017-09-06 16:34:36 +0100

    Fixed library sidebar scroll events not bubbling through child items (so now library sidebar will vertically scroll with mousewheel whever the mouse is located on the sidebar)

--- MedLaunch/MainWindow.xaml.cs
+++ MedLaunch/MainWindow.xaml.cs
@@ -60,6 +60,7 @@ using System.Windows.Threading;
 using MedLaunch._Debug.ScrapeDB.ReplacementDocs;
 using MedLaunch.Classes.MednaNet;
 using ucon64_wrapper;
+using System.Windows.Interactivity;
 
 namespace MedLaunch
 {
@@ -6673,6 +6674,28 @@ namespace MedLaunch
         #endregion
     }
 
+    // Used on sub-controls of an expander to bubble the mouse wheel scroll event up 
+    public sealed class BubbleScrollEvent : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseWheel += AssociatedObject_PreviewMouseWheel;
+        }
 
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseWheel -= AssociatedObject_PreviewMouseWheel;
+            base.OnDetaching();
+        }
+
+        void AssociatedObject_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+            var e2 = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+            e2.RoutedEvent = UIElement.MouseWheelEvent;
+            AssociatedObject.RaiseEvent(e2);
+        }
+    }
 
 }
