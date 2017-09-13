ec0e59d8b0fb719ad3a22518324281c7e7db522c
ASNiVOR <coding@asnitech.co.uk>
2017-09-13 11:13:54 +0100

    Appveyor version pre-build

---
+++ Build/AppveyorVersioning.ps1
@@ -0,0 +1,48 @@
+﻿write-host "Starting MedLaunch pre-build scripts.."
+
+# get current directory (this will be something/something/Build)
+# so root would be $loc\..\
+$loc = Split-Path $script:MyInvocation.MyCommand.Path
+$root = (get-item $loc).parent.FullName
+
+# get fileversion from assemblyinfo
+$asi = get-content $root\MedLaunch\Properties\AssemblyInfo.cs
+foreach ($line in $asi)
+{
+    if ($line -like '*AssemblyFileVersion(*')
+    {
+        $version = $line.Replace("[assembly: AssemblyFileVersion(", "").Replace(")]", "").Replace("`"", "").Replace(".", "_")
+    }
+}
+write-host "Detected file version: $version"
+
+# run only if this is within appveyor
+if ($env:APPVEYOR -eq $true)
+{
+    write-host "Appveyor detected"
+    if ($env:APPVEYOR_REPO_BRANCH -eq "dev")
+    {
+        $buildNo = $env:APPVEYOR_BUILD_NUMBER
+        write-host "dev branch detected"
+        $filename = "MedLaunch_v$($version)-DEVBUILD_$($buildNo).zip"
+        $buildVer = $filename.Replace(".zip", "")
+        # set enviroment version
+        $env:APPVEYOR_BUILD_VERSION = $buildVer
+    }
+    if ($env:APPVEYOR_REPO_BRANCH -eq "master")
+    {
+        $buildNo = $env:APPVEYOR_BUILD_NUMBER
+        write-host "master branch detected"
+        $filename = "MedLaunch_v$($version).zip"
+        $buildVer = "$($filename.Replace(".zip", ""))-MASTER_$($buildNo)"
+        # set enviroment version
+        $env:APPVEYOR_BUILD_VERSION = $buildVer
+    }
+}
+else
+{
+    write-host "No Appveyor detected - ignoring branches"
+    $filename = "MedLaunch_v$($version).zip"
+}
+
+write-host "Artifact name will be: $filename"
